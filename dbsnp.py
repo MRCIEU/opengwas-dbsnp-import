157e357d5f99c57c08d9de3c577c8f81c70d8107
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2025-04-08 09:41:00 +0100

    feat: Process merged snps

--- dbsnp.py
+++ dbsnp.py
@@ -1,5 +1,6 @@
 import os
 import redis
+import subprocess
 import time
 
 from dotenv import load_dotenv
@@ -9,11 +10,13 @@ load_dotenv()
 
 class DBSNP:
     def __init__(self, redis_key: str):
-        self.r = redis.Redis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'], password=os.environ['REDIS_PASS'], db=os.environ['REDIS_DB'])
+        self.r = redis.Redis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'], password=os.environ['REDIS_PASS'], db=os.environ['REDIS_DB'], decode_responses=True)
         self.redis_key = redis_key
         self.last_time = time.time()
 
-    def import_batch(self, rsid_and_chrpos: dict) -> None:
+    def import_batch(self, rsid_and_chrpos: dict) -> int:
+        if len(rsid_and_chrpos) == 0:
+            return 0
         self.r.hset(self.redis_key, mapping=rsid_and_chrpos)
         return self.r.hlen(self.redis_key)
 
@@ -27,6 +30,10 @@ class DBSNP:
         parts = line.rstrip('\n').split()
         return int(parts[2][2:]), f"{parts[0]}:{parts[1]}"
 
+    def process_merged_line(self, line: str) -> tuple:
+        parts = line.rstrip('\n').split()
+        return parts[0], parts[1]
+
     def timer_lap(self):
         current_time = time.time()
         elapsed_time = current_time - self.last_time
@@ -34,20 +41,54 @@ class DBSNP:
         return elapsed_time
 
 
+def _count_lines(filename: str) -> int:
+    return int(subprocess.getoutput("wc -l %s" % filename).split()[0])
+
+
 if __name__ == '__main__':
     wd = os.environ['WORKING_DIR']
     dbsnp = DBSNP('dbsnp_157')
+
+    t = time.time()
+
     count = 0
     batch_size = 1_000_000
     rsid_and_chrpos = {}
-    t = time.time()
+    n_lines = _count_lines(wd + '/chr_pos_rsid.txt')
     with open(wd + '/chr_pos_rsid.txt', 'r') as f:
         for line in f:
             rsid_without_rs, chrpos = dbsnp.process_chr_pos_rsid_line(line)
             rsid_and_chrpos[rsid_without_rs] = chrpos
             count += 1
-            if count % batch_size == 0:
+            if count % batch_size == 0 or count == n_lines:
                 print(count // batch_size, dbsnp.timer_lap())
                 dbsnp.import_batch(rsid_and_chrpos)
                 rsid_and_chrpos = {}
-                print(dbsnp.timer_lap())
+                print('batch ending at line', count, dbsnp.timer_lap())
+
+    count = 0
+    batch_size = 100_000
+    old_rsids = []
+    new_rsids = []
+    rsid_and_chrpos = {}
+    n_lines = _count_lines(wd + '/merged.txt')
+    with open(wd + '/merged.txt', 'r') as f:
+        for line in f:
+            old_rsid, new_rsid = dbsnp.process_merged_line(line)
+            old_rsids.append(old_rsid)
+            new_rsids.append(new_rsid)
+            count += 1
+            if count % batch_size == 0 or count == n_lines:
+                print(count // batch_size, dbsnp.timer_lap())
+                for i, chrpos in enumerate(dbsnp.get_batch(new_rsids)):
+                    if chrpos is not None:
+                        rsid_and_chrpos[old_rsids[i]] = chrpos
+                print(len(rsid_and_chrpos), dbsnp.timer_lap())
+                for i, chrpos in enumerate(dbsnp.get_batch(old_rsids)):
+                    if chrpos is not None:
+                        print(old_rsids[i])
+                dbsnp.import_batch(rsid_and_chrpos)
+                old_rsids = []
+                new_rsids = []
+                rsid_and_chrpos = {}
+                print('batch ending at line', count, dbsnp.timer_lap())
